d18b17e70db94e8fe50895277f7623c8c214a4bd
Felipe Manso <felipemanso@outlook.com>
2018-10-18 16:53:48 -0300

    Caelum 9.7

---
+++ Caelum-9.7/src/entities/ContaCorrente.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class ContaCorrente extends Conta {
+
+	private double limiteEmprestimo;
+
+	public ContaCorrente(String titular, int numero, String agencia, double saldo, String dataAbertura,
+			double limiteEmprestimo) {
+		super(titular, numero, agencia, saldo, dataAbertura);
+		this.limiteEmprestimo = limiteEmprestimo;
+	}
+
+	public double getLimiteEmprestimo() {
+		return limiteEmprestimo;
+	}
+
+	public void setLimiteEmprestimo(double limiteEmprestimo) {
+		this.limiteEmprestimo = limiteEmprestimo;
+	}
+	
+	
+}
